07c494c1106020842095e7834fe3dd745729ce24
Yonn Argelia <yonnarge@student.21-school.ru>
2022-06-29 12:50:37 +0300

    s21_matrix.h

---
+++ src/s21_matrix.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright (с) 2022 Student of School 21:
+ * Yonn Argelia
+ * yonnarge@student.21-school.ru
+ *
+ * The s21_matrix.h library is intended for processing numerical matrices
+ * in the C programming language.
+ *
+ * Библиотека s21_matrix.h предназначения для обработки числовых матриц
+ * на языке программировния C.
+ */
+
+#ifndef SRC_S21_MATRIX_H_
+#define SRC_S21_MATRIX_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// Возвращаемые значения функции s21_eq_matrix
+#define SUCCESS 1
+#define FAILURE 0
+
+// Структура матрицы
+typedef struct matrix_struct {
+  double** matrix;
+  int rows;
+  int columns;
+} matrix_t;
+
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_transpose(matrix_t *A, matrix_t *result);
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_determinant(matrix_t *A, double *result);
+int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+
+#endif  // SRC_S21_MATRIX_H_
