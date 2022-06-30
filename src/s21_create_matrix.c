a5d4bc908c17d5b8cfdc054a6adc6bbe062fef8f
Yonn Argelia <yonnarge@student.21-school.ru>
2022-07-01 00:20:51 +0300

    Add s21_create_matrix.c. NO TEST

--- src/s21_create_matrix.c
+++ src/s21_create_matrix.c
@@ -8,7 +8,7 @@
 #include "s21_matrix.h"
 
 /**
- * Функция s21_create_matrix создаёт матрицу размером rows на columns
+ * @brief Функция создаёт матрицу размером rows на columns
  * по указателю result.
  * @param rows - количество рядов,
  * @param columns - количество колонок,
@@ -18,7 +18,39 @@
  *          2 - Ошибка вычисления.
  */
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
-    int resulting_code = 0;
-
-    return (resulting_code);
+    /* Объявление переменной для возвращаемого кода ошибки */
+    int errcode = 0;
+    if (NULL == result) {
+        /* Код ошибки 2, если указатель result - некорректный */
+        errcode = 2;
+    } else if (rows < 1 || columns < 1) {
+        /* Код ошибки 1, если rows или columns - некорректные */
+        errcode = 1;
+    } else {
+        /* Инициализация переменной, содержащий размер
+         * массива указателей на ряды создаваемой матрицы */
+        size_t arr_pointers = rows * sizeof(double *);
+        /* Инициализация rows и columns в создаваемой матрице */
+        result->rows = rows;
+        result->columns = columns;
+        /* Выделение памяти для содержимого матрицы осуществляется
+         * одним вызовом calloc одновременно для массива указателей на ряды
+         * и всех элементов матрицы */
+        result->matrix = (double **)calloc(arr_pointers + \
+                                           rows * columns * sizeof(double));
+        if (NULL == result->matrix) {
+            /* Код ошибки 2, если память для содержимого матрицы не была выделена */
+            errcode = 2;
+        } else {
+            /* Инициализация переменной, содержащий указатель
+             * на начало массива элементов матрицы */
+            double *start = (double *)((char *)result->matrix + arr_pointers);
+            /* Инициализируем указатели на ряды от начала массива элементов */
+            for (int i = 0; i < rows; ++i) {
+                result->matrix[i] = start + columns * i;
+            }
+        }
+    }
+    /* Возврат кода ошибки */
+    return (errcode);
 }
