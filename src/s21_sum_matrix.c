68f2afa0c133255d27020217d2f23127cdc493f4
Yonn Argelia <yonnarge@student.21-school.ru>
2022-07-01 21:37:54 +0300

    Add s21_sum_matrix

--- src/s21_sum_matrix.c
+++ src/s21_sum_matrix.c
@@ -18,7 +18,20 @@
  *          2 - Ошибка вычисления.
  */
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-    int resulting_code = 0;
+    int errcode = 0;
 
-    return (resulting_code);
+    // Добавить проверку адекватности матриц через отдельную чек-функцию
+
+    if (NULL == A || NULL == B || NULL == result) {
+        errcode = 1;
+    } else if (A->rows != B->rows || A->columns != B->columns) {
+        errcode = 2;
+    } else {
+        for (int i = 0; i < result->columns; ++i) {
+            for (int j = 0; j < result->rows; ++j) {
+                result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+            }
+        }
+    }
+    return (errcode);
 }
